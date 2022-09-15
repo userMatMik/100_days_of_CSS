4652a41e13f15913e5b2a78c198e09e6531a23a9
userMatMik <106982809+userMatMik@users.noreply.github.com>
2022-09-15 14:06:24 +0200

    Add files via upload

---
+++ captcha_generator/README.md
@@ -0,0 +1,21 @@
+# Captcha Generator App
+
+![Design preview for the password generator app coding challenge](./img/captcha.png)
+
+## Welcome! 👋
+
+Thanks for checking out this small project. I created this component as a short break from bigger projects that Im currently working on.
+Also this project is a part of my challenge - 100 days of coding :)
+
+This project was fun and I was supprised how fast I build that generator and ofcourse I learn some new things for example:
+
+- how to disadble text from copying
+
+Hope you enjoy this small project.
+
+- Live Site URL: (https://wonderful-tarsier-704f14.netlify.app//)
+
+## Built with
+
+- SCSS
+- Vanilla JS
--- captcha_generator/script.js
+++ captcha_generator/script.js
@@ -1,43 +1,78 @@
 const captchaContainer = document.querySelector('#preview');
 const reloadBtn = document.querySelector('#reload');
+const submitBtn = document.querySelector('#submit');
+const captchaInput = document.querySelector('input[type="text"]')
 
 const listOfChars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-const capchaLength = 9
+const capchaLength = 9;
+let captcha = '';
 
+const validateCaptcha = (captcha) => {
+    console.log(captcha);
+    const errorMessages = {
+        1: 'Input cannot be empty.',
+        2: 'Wrong text. Please try again.',
+    }
+    const captchaGenerator = document.querySelector('#app');
+    const enteredText = captchaInput.value;
+    const errorMsg = document.querySelector('#error-message');
+    const successMsg = document.querySelector('#success');
+    if (enteredText.length === 0) {
+        errorMsg.textContent = errorMessages[1];
+        errorMsg.classList.add('show-error');
+    }
+    if(enteredText.length !== 0 && enteredText !== captcha) {
+        errorMsg.textContent = errorMessages[2];
+        errorMsg.classList.add('show-error');
+    }
+    if(enteredText === captcha) {
+        captchaGenerator.classList.add('hide-app');
+        successMsg.classList.add('show-success');
+    }
+}
 
 const randomNumber = (minNum, maxNum) => {
     return Math.floor(Math.random() * (maxNum - minNum + 1) + minNum);
 } 
 
-
 const renderCaptcha = (captcha) => {
    [...captcha].forEach((character) => {
        const spanEl = document.createElement('span');
-       spanEl.classList.add('test');
        spanEl.textContent = character;
-       spanEl.style.transform = `translateY(${randomNumber(-15, 15)}px) rotate(${randomNumber(-30, 30 )}deg)`
+       spanEl.style.transform = `translateY(${randomNumber(-15, 15)}px) rotate(${randomNumber(-30, 30 )}deg)`;
        captchaContainer.appendChild(spanEl);
    })
 }
 
 const generateCaptcha = () => {
-    let captcha =''
+    captcha = '';
     for (let i = 0; i < capchaLength; i++) {
-        // const randomIndex = Math.floor(Math.random() * listOfChars.length); 
         const randomIndex = randomNumber(0, listOfChars.length - 1); 
         captcha += listOfChars[randomIndex];
     }
-
     renderCaptcha(captcha);
 }
 
 reloadBtn.addEventListener('click', () => {
     captchaContainer.textContent = '';
+    document.querySelector('input[type="text"]').value = ""
     generateCaptcha();
 });
 
+submitBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    validateCaptcha(captcha);
+})
 
-generateCaptcha()
+submitBtn.addEventListener('keypress', (event) => {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        validateCaptcha(captcha);
+    }
+})
+
+
+generateCaptcha();
       
   
 
